5cba4426e22598c538da3759cdbb52f73086d37c
Lee Witek <neomech@gmail.com>
2016-02-29 13:48:07 +0000

    added command line options

--- dlo.go
+++ dlo.go
@@ -1,8 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
 
-func editCommandHandler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		http.ServeFile(w, r, "www/dloview.html")
 	} else {
@@ -11,7 +17,39 @@ func editCommandHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+type Config struct {
+	ListenPort int
+	DataFolder string
+	WWWfolder  string
+}
+
+var config Config
+
+func init() {
+	flag.IntVar(&config.ListenPort, "p", 80, "http listen port")
+	flag.StringVar(&config.DataFolder, "d", "./data", "data folder location")
+	flag.StringVar(&config.WWWfolder, "w", "./www", "www folder location")
+
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "Usage: dlo [options]")
+		flag.PrintDefaults()
+	}
+}
+
+func exitOnError(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+// Linux: to listen on ports <1024: sudo setcap cap_net_bind_service=+ep dlo
 func main() {
-	http.HandleFunc("/", editCommandHandler)
-	http.ListenAndServe(":8080", nil)
+	flag.Parse()
+
+	http.HandleFunc("/", handler)
+
+	log.Printf("Listening on port %d", config.ListenPort)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", config.ListenPort), nil)
+	exitOnError(err)
 }

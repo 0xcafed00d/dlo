0791b49e66d8ec12b91ec151443b55f2b557e335
Lee Witek <neomech@gmail.com>
2016-03-04 17:24:12 +0000

    implemented FileIndex

--- fileindex.go
+++ fileindex.go
@@ -22,12 +22,33 @@ func MakeFileIndex(dataRoot string) *FileIndex {
 	return &fi
 }
 
-func (fi *FileIndex) RefeshFileCount() error {
-	return nil
+func (fi *FileIndex) RefeshFileCount() {
+	fi.Mutex.Lock()
+	defer fi.Unlock()
+
+	fi.fileCount = 0
+
+	folders, err := filepath.Glob(filepath.Join(fi.dataRoot, "*"))
+	if err != nil {
+		return
+	}
+
+	if len(folders) == 0 {
+		return
+	}
+	files, err := filepath.Glob(filepath.Join(folders[len(folders)-1], "*"))
+
+	fi.fileCount = uint64(((len(folders) - 1) * 1000) + len(files))
 }
 
 func (fi *FileIndex) MakeDummyFiles(count int) error {
-
+	for n := 0; n < count; n++ {
+		index := fi.ReserveFileIndex()
+		err := fi.StoreFile(index, fmt.Sprintf("file index %v", index))
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
@@ -38,11 +59,25 @@ func (fi *FileIndex) makeFileName(index uint64) string {
 }
 
 func (fi *FileIndex) StoreFile(index uint64, text string) error {
-	f, err := os.Create(fi.makeFileName)
-	if err != nil {
 
+	// first file in a set? create folder.
+	if index%1000 == 0 {
+		err := os.MkdirAll(filepath.Join(fi.dataRoot, fmt.Sprintf("%05v", index/1000)), os.ModePerm)
+		if err != nil {
+			return err
+		}
 	}
 
+	f, err := os.Create(fi.makeFileName(index))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = fmt.Fprint(f, text)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -50,6 +85,7 @@ func (fi *FileIndex) ReserveFileIndex() uint64 {
 	fi.Mutex.Lock()
 	defer fi.Unlock()
 
+	n := fi.fileCount
 	fi.fileCount++
-	return fi.fileCount
+	return n
 }
--- fileindex_test.go
+++ fileindex_test.go
@@ -14,3 +14,13 @@ func TestFileIndex(t *testing.T) {
 	assert.Equal(t, fi.makeFileName(1000), "data/00001/000")
 	assert.Equal(t, fi.makeFileName(12345678), "data/12345/678")
 }
+
+func TestStore(t *testing.T) {
+	fi := MakeFileIndex("testdata")
+
+	err := fi.MakeDummyFiles(3999)
+	assert.Nil(t, err)
+
+	fi.RefeshFileCount()
+	assert.Equal(t, fi.fileCount, uint64(3999))
+}

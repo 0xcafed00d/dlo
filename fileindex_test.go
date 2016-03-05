b475b6f4869b59183ddff064ad4e6deb86151ca1
Lee Witek <neomech@gmail.com>
2016-03-05 23:31:52 +0000

    more tests

--- fileindex_test.go
+++ fileindex_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+	"strconv"
 	"testing"
+	"time"
 
 	"github.com/simulatedsimian/assert"
 )
@@ -15,12 +19,38 @@ func TestFileIndex(t *testing.T) {
 	assert.Equal(t, fi.makeFileName(12345678), "data/12345/678")
 }
 
+func exist(fname string) bool {
+	_, err := os.Stat(fname)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
 func TestStore(t *testing.T) {
-	fi := MakeFileIndex("testdata")
+
+	path := filepath.Join("/tmp", "testdata", strconv.FormatInt(time.Now().UnixNano(), 10))
+
+	fi := MakeFileIndex(path)
 
 	err := fi.MakeDummyFiles(3999)
 	assert.Nil(t, err)
 
 	fi.RefeshFileCount()
 	assert.Equal(t, fi.fileCount, uint64(3999))
+
+	assert.Equal(t, exist(filepath.Join(path, "00003", "999")), false)
+	index := fi.ReserveFileIndex()
+	err = fi.StoreFile(index, "test file")
+	assert.Nil(t, err)
+	assert.Equal(t, exist(filepath.Join(path, "00003", "999")), true)
+
+	assert.Equal(t, exist(filepath.Join(path, "00004", "000")), false)
+	index = fi.ReserveFileIndex()
+	err = fi.StoreFile(index, "test file")
+	assert.Nil(t, err)
+	assert.Equal(t, exist(filepath.Join(path, "00004", "000")), true)
+
 }

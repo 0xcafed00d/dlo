0162847933602ea095a2bb51f2e4d688035dfbc2
Lee Witek <neomech@gmail.com>
2016-03-10 20:19:43 +0000

    added templated html files

--- dlo.go
+++ dlo.go
@@ -3,24 +3,31 @@ package main
 import (
 	"flag"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 )
+
+var EditTemplate *template.Template
+var ViewTemplate *template.Template
 
 func handlerEditLetter(w http.ResponseWriter, r *http.Request) {
 	log.Println("handlerEditLetter")
-	http.ServeFile(w, r, "www/dloedit.html")
+	logOnError(EditTemplate.Execute(w, ""))
 }
 
 func handlerPostLetter(w http.ResponseWriter, r *http.Request) {
-	log.Println("handlerPostLetter:", r.Form["messagetext"])
-	http.ServeFile(w, r, "www/dloview.html")
+	data := r.Form["messagetext"][0]
+	log.Println("handlerPostLetter:", data)
+	logOnError(ViewTemplate.Execute(w, data))
 }
 
 func handlerViewRandom(w http.ResponseWriter, r *http.Request) {
 	log.Println("handlerViewRandom")
-	http.ServeFile(w, r, "www/dloview.html")
+	data := "this is some text"
+	logOnError(ViewTemplate.Execute(w, data))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -66,13 +73,29 @@ func exitOnError(err error) {
 	}
 }
 
+func logOnError(err error) {
+	if err != nil {
+		log.Println(err)
+	}
+}
+
 // Linux: to listen on ports <1024: sudo setcap cap_net_bind_service=+ep dlo
 func main() {
 	flag.Parse()
 
+	t, err := template.ParseFiles(
+		filepath.Join(config.WWWfolder, "dloedit.html"),
+		filepath.Join(config.WWWfolder, "dloview.html"),
+	)
+	exitOnError(err)
+	log.Println(t.DefinedTemplates())
+
+	EditTemplate = t.Lookup("dloedit.html")
+	ViewTemplate = t.Lookup("dloview.html")
+
 	http.HandleFunc("/", handler)
 
 	log.Printf("Listening on port %d", config.ListenPort)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", config.ListenPort), nil)
+	err = http.ListenAndServe(fmt.Sprintf(":%d", config.ListenPort), nil)
 	exitOnError(err)
 }
